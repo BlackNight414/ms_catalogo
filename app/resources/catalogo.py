c7a7b26e9e70f059ebfa9d2a8e6c6dde83bdf31e
BlackNight414 <cristianariel375@gmail.com>
2024-10-24 13:58:32 -0300

    Se agregan rutas de catalogo

--- app/resources/catalogo.py
+++ app/resources/catalogo.py
@@ -3,6 +3,7 @@ from flask import jsonify, make_response, request
 
 from app.models import Producto
 from app.services import CatalogoService
+from app import cache
 
 catalogo = Blueprint('catalogo', __name__)
 
@@ -16,9 +17,13 @@ def get_all():
 
 @catalogo.route('/get_by_id/<int:id>', methods=['GET'])
 def get_by_id(id: int):
-    print("SOY CATALOGO :)")
-    producto = catalogo_service.get_by_id(id)
-    print(producto)
+
+    # Duda: ¿Cache idealmente va en las rutas o en los métodos de services?
+    producto = cache.get(f'producto_id_{id}')
+    if producto is None:
+        producto = catalogo_service.get_by_id(id)
+        cache.set(f'producto_id_{producto.id}', producto, timeout=60)
+
     if producto:
         resp = jsonify({
             'id': producto.id,
@@ -26,9 +31,9 @@ def get_by_id(id: int):
             'precio': producto.precio,
             'activado': producto.activado
         })
-        return make_response(resp)
+        return make_response(resp), 200
     else:
-        return make_response(jsonify(f'El producto id={id} no existe.')), 200
+        return make_response(jsonify({'msg': 'NOT FOUND'})), 200
     
 @catalogo.route('/add', methods=['POST'])
 def add():
